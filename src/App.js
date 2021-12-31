3203b46bfba503fc027b9242319e3349cd83cc9a
nitinsaad <nitin.saad1995@gmail.com>
2021-12-31 14:01:41 +0530

    in develop

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Git upload project origin master
+        Git upload project origin master development branch
       </header>
     </div>
   );
