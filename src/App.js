8f60c468f840eb170471bdf635aea1a8725dcbde
nitinsaad <nitin.saad1995@gmail.com>
2021-12-31 13:57:07 +0530

    git push origin master

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Git upload project
+        Git upload project origin master
       </header>
     </div>
   );
